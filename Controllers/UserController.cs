d84ed7ab490437cb9eb8a9bf2ddb1ebf6990ab84
Mohammad Mousharraf Anwar <manwar@contractresources.com>
2023-03-15 15:35:33 +0400

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BankingAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IHttpActionResult GetTest()
+        {
+            //var studet = st.FirstOrDefault((s) => s.stuID == stuid);
+            //if (studet == null)
+            //{
+                //return NotFound();
+            //}
+            return Ok("Anwar");
+        }
+    }
+}
